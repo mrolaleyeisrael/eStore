086c9de6a2c403c7ce209c930d3888b40e1269e6
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2022-05-09 12:44:38 +0100

    Boiler plate

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className=' bg-blue-500 text-white shadow-lg fixed w-full top-0 z-20'>
+      <nav className=' px-2 py-5 flex flex-row justify-between items-center mx-auto md:px-10 '>
+        <h1 className=' text-2xl font-bold'>World Universities</h1>
+        <div className=' md:space-x-10 hidden justify-between md:flex text-md font-bold'>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <div>
+          <h2>Dark Mode</h2>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
